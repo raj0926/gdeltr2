05fa6567a4cccef10b10ce30f524e4d137f13102
Alex Bresler <abresler@asbcllc.com>
2016-06-04 14:52:07 -0400

    fix adm

--- R/gdelt_event_gkg.R
+++ R/gdelt_event_gkg.R
@@ -1391,10 +1391,24 @@ get_gdelt_url_data <-
 
     }
 
-    if ('id.adm2code.actor.1' %in% names(gdelt_data)) {
+    if ('id.adm1code.actor.2' %in% names(gdelt_data)) {
       gdelt_data <-
         gdelt_data %>%
         mutate(id.adm1code.actor.2 = id.adm1code.actor.2 %>% as.character())
+
+    }
+
+    if ('id.adm2code.actor.1' %in% names(gdelt_data)) {
+      gdelt_data <-
+        gdelt_data %>%
+        mutate(id.adm2code.actor.1 = id.adm2code.actor.1 %>% as.character())
+
+    }
+
+    if ('id.adm2code.actor.2' %in% names(gdelt_data)) {
+      gdelt_data <-
+        gdelt_data %>%
+        mutate(id.adm2code.actor.2 = id.adm2code.actor.2 %>% as.character())
 
     }
 
